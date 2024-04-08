27966520ecb7c73659052bfa5fc9a46b02122d64
AnushaS10 <161955708+AnushaS10@users.noreply.github.com>
2024-04-08 12:36:20 +0530

    Update streamlit_app.py
    fixed typo and added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ streamlit.header('Breakfast Favorites')
 streamlit.text(' 🥣 Omega 3 & blueberry Oatmeal')
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
-streamlit.text(' 🥑🍞 Avacado Toast')
+streamlit.text(' 🥑🍞 Avacado Toast ')
 
